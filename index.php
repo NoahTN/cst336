d37da8cedd90c8fdfbf162b48990bd22ef98ad52
Noah Tolentino Nguyen <ntolentino-nguyen@csumb.edu>
2018-09-16 17:02:08 +0000

    Added index

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
-Portfolio Lab
-https://cst336noahtn.herokuapp.com/labs/portfolio
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <div>
+            Portfolio Lab
+            <br>
+            <a href="https://cst336noahtn.herokuapp.com/labs/portfolio">https://cst336noahtn.herokuapp.com/labs/portfolio</a>
+        </div>
+        <br>
+         <div>
+            Homework 1
+            <br>
+            <a href=" https://cst336noahtn.herokuapp.com/homework/1"> https://cst336noahtn.herokuapp.com/homework/1</a>
+        </div>
+        <br>
+         <div>
+            Slot Machine Lab
+            <br>
+            <a href="https://cst336noahtn.herokuapp.com/labs/777">https://cst336noahtn.herokuapp.com/labs/777</a>
+        </div>
+    
+    </body>
 
-Homework 1
-https://cst336noahtn.herokuapp.com/homework/1
-
-Slot Machine Lab
-https://cst336noahtn.herokuapp.com/labs/777
+    
+</html>
