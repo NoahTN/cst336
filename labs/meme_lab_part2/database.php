6798e0be1196a9111e3b1ab0a1038a7ae2291f05
Noah Tolentino Nguyen <ntolentino-nguyen@csumb.edu>
2018-11-08 04:46:00 +0000

    Fix database for heroku

--- labs/meme_lab_part2/database.php
+++ labs/meme_lab_part2/database.php
@@ -14,7 +14,7 @@
             $host = "us-cdbr-iron-east-01.cleardb.net";
             $username = "b00cd32a10cfc9";
             $password = "3f88949a";
-            $dbname = "Memebase"; 
+            $dbname = "heroku_62f59227e5454ba"; 
         }
         // Create connection
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
