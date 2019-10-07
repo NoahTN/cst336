b2bd2ca9b374cad61e41d47c0a086c9063b6fda2
NoahTN <42818569+NoahTN@users.noreply.github.com>
2019-10-06 21:43:53 -0700

    Update credential env set

--- projects/final_proj/getAvailableLangs.php
+++ projects/final_proj/getAvailableLangs.php
@@ -28,7 +28,7 @@ foreach ($records as $record) {
 
 # Your Google Cloud Platform project ID
 $projectId = env('PROJ_ID');
-putenv('GOOGLE_APPLICATION_CREDENTIALS = {json_decode(env('GOOGLE_CREDENTIALS'), true)}');
+putenv('GOOGLE_APPLICATION_CREDENTIALS='.json_decode(env('GOOGLE_CREDENTIALS'), true));
 
 $translate = new TranslateClient();
 
